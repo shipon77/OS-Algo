6f333b4e7ab9fd3c3402591b843f00d78e2c761c
Shipon Ahmed <110889944+shipon77@users.noreply.github.com>
2022-08-09 13:58:53 +0600

    Add files via upload

---
+++ os_mvt.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+    int m=0,m1=0,m2=0,p,count=0,i;
+    printf("enter the memory capacity:");
+    scanf("%d",&m);
+    printf("enter the number of procss:");
+    scanf("%d",&p);
+
+    for(i=0;i<p;i++){
+        printf("\nenter memory req for%d:",i+1);
+        scanf("%d",&m1);
+        count=count+m1;
+
+        if(m1<=m){
+            if(count==m)
+                printf("there is no further memory require:");
+        printf("the memory allocated for process%d is:%d",i+1,m);m2=m-m1;
+        printf("\nremaining memory is:%d",m2);
+        m=m2;
+        }
+        else{
+            printf("memory is not allocated for process%d",i+1);
+        }
+        printf("\nexternal fragmentation for this process is:%d",m2);
+        }
+        getch();
+    }
+
+
